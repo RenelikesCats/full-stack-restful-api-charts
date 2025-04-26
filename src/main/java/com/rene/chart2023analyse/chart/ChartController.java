a9b5988b325dadc713424a3d445d9183d812f07f
Rene <rene.talukder@icloud.com>
2025-04-26 20:29:01 +0200

    uri updated

--- src/main/java/com/rene/chart2023analyse/chart/ChartController.java
+++ src/main/java/com/rene/chart2023analyse/chart/ChartController.java
@@ -4,12 +4,13 @@ import com.rene.chart2023analyse.chart.dto.ArtistTrackDayMonthYearStreams;
 import com.rene.chart2023analyse.chart.dto.NewTrack;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Stream;
 
 @RestController
-@RequestMapping("spotify2023chart")
+@RequestMapping("chart2023/tracks")
 public class ChartController {
     private final ChartService chartService;
 
@@ -18,43 +19,44 @@ public class ChartController {
     }
 
     @Operation(summary = "Finds all (includes name, song title, date, streams)")
-    @GetMapping("/tracks")
+    @GetMapping()
     public Stream<ArtistTrackDayMonthYearStreams> findAll() {
         return chartService.findAll().stream().map(ArtistTrackDayMonthYearStreams::new);
     }
 
     @Operation(summary = "Returns tracks with no featured artist(s)")
-    @GetMapping("tracks/solo")
+    @GetMapping("solo")
     public Stream<ArtistTrackDayMonthYearStreams> findOnlySoloArtists() {
         return chartService.findOnlySoloArtists().stream().map(ArtistTrackDayMonthYearStreams::new);
     }
 
     @Operation(summary = "Returns all tracks with given specific year")
-    @GetMapping("/tracks/filterByYear")
+    @GetMapping("filterByYear")
     public Stream<ArtistTrackDayMonthYearStreams> findByReleasedYear(@RequestParam int releaseYear) {
         return chartService.findByReleasedYear(releaseYear).stream().map(ArtistTrackDayMonthYearStreams::new);
     }
 
     @Operation(summary = "Find all tracks by given platform and given rank")
-    @GetMapping("/tracks/{platform}/rank")
+    @GetMapping("{platform}/rank")
     public Stream<ArtistTrackDayMonthYearStreams> findAllByPlatform_And_ChartsByRank(@PathVariable String platform, @RequestParam int rank) {
         return chartService.findAllByPlatform_And_ChartsByRank(platform, rank).stream().map(ArtistTrackDayMonthYearStreams::new);
     }
 
     @Operation(summary = "Delete a track by given track id")
-    @DeleteMapping("/tracks/{id}")
+    @DeleteMapping("{id}")
     public void deleteById(@PathVariable long id) {
         chartService.delete(id);
     }
 
     @Operation(summary = "Updates the chart rank for an existing track in DB")
-    @PutMapping("/tracks/{id}/{platform}/setrank")
+    @PutMapping("{id}/{platform}/setrank")
     public void updateChartRank(@PathVariable long id, @PathVariable String platform, @RequestParam int rank) {
         chartService.updateChartRank(id, platform, rank);
     }
 
     @Operation(summary = "Create new track")
-    @PostMapping("/tracks/new")
+    @PostMapping("new")
+    @ResponseStatus(HttpStatus.CREATED)
     public long create(@RequestBody @Valid NewTrack newTrack) {
         return chartService.create(newTrack);
     }
